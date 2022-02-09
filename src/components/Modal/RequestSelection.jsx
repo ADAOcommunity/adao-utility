182eb43b24b623c5e59dfbdafc109c569446c809
rodrigomd94 <rodrigomd94@gmail.com>
2022-02-09 16:13:55 -0600

    added restriction of max amount of assset to send to treasury based on amount in wallet

--- src/components/Modal/RequestSelection.jsx
+++ src/components/Modal/RequestSelection.jsx
@@ -43,7 +43,7 @@ const toUnit = (amount, decimals = 6) => {
 };
 
 const addressToBech32 = async () => {
-  if(window.cardano.isEnabled()) {
+  if (window.cardano.isEnabled()) {
     await Loader.load();
     const address = (await window.cardano.getUsedAddresses())[0];
     return Loader.Cardano.Address.from_bytes(
@@ -68,27 +68,27 @@ const blockfrostRequestt = (endpoint, headers, body) => { // LOL - This is no lo
   }).then((res) => res.json());
 }
 
-function hex_to_ascii(str1)
- {
-	var hex  = str1.toString();
-	var str = '';
-	for (var n = 0; n < hex.length; n += 2) {
-		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
-	}
-	return str;
- }
+function hex_to_ascii(str1) {
+  var hex = str1.toString();
+  var str = '';
+  for (var n = 0; n < hex.length; n += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+  }
+  return str;
+}
 
 const getImageHash = async (asset) => {
   let a = await asset
   let p = a[0].policy_id
   let n = a[0].asset_name_ascii
-  return a[0].minting_tx_metadata.json[p][n].image.replace("ipfs://","")
+  return a[0].minting_tx_metadata.json[p][n].image.replace("ipfs://", "")
   //console.log(a)
   //return "QmWo4HYh4wwnZJ8s7Kz6Z7q76P6YLMyhsPy12rJSTewBqZ"
 }
 
 const getAllAssets = async (addr) => {
-  if(addr === '') return []
+  if (addr === '') return []
+  var quantities = {}
   const addressInfo = await blockfrostRequest(`/address_info?_address=${addr}`)
   console.log(addressInfo)
   const utxo_set = addressInfo[0].utxo_set
@@ -98,17 +98,20 @@ const getAllAssets = async (addr) => {
     let asset_list = utxo.asset_list
     // console.log(asset_list)
     asset_list.forEach((amnt) => {
+      quantities[amnt.policy_id + amnt.asset_name] = amnt.quantity
       assetsNameList.push(amnt.policy_id + amnt.asset_name)
     })
   })
+  console.log(quantities)
 
   const assets = async () => {
     return Promise.all(assetsNameList.map(async (assetEncoded) => {
       let policy = assetEncoded.slice(0, 56)
       let tokenName = Buffer.from(assetEncoded.slice(56), "hex")
-      const asset = blockfrostRequestt(`/asset_info?_asset_policy=${policy}&_asset_name=${fromAscii(tokenName)}`) // break up by bytes and name.
-      console.log(getImageHash(asset));
-      return {name: tokenName, image: await getImageHash(asset), encodedFullName: assetEncoded}
+      let quantity = quantities[assetEncoded]
+      const asset = await blockfrostRequestt(`/asset_info?_asset_policy=${policy}&_asset_name=${fromAscii(tokenName)}`) // break up by bytes and name.
+      console.log(asset);
+      return { name: tokenName, image: await getImageHash(asset), encodedFullName: assetEncoded, quantity: quantity }
       /*if(asset.onchain_metadata && asset.onchain_metadata.name && asset.onchain_metadata.image) {
         return { name: asset.onchain_metadata.name, image: asset.onchain_metadata.image, encodedFullName: assetEncoded}
       }
@@ -118,7 +121,8 @@ const getAllAssets = async (addr) => {
       if (asset.token_registry_metadata) {
 
       }*/ // TODO we need to finish this for asset display of images..
-  }))}
+    }))
+  }
   // assets.forEach( asset => asset.then((val) => {console.log(val)}))
   return assets()
 }
@@ -128,138 +132,147 @@ const getAllAssets = async (addr) => {
 const isBrowser = () => typeof window !== "undefined";
 
 const RequestSelection = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const addRequest = useStoreActions(
-      (actions) => actions.requests.addRequest
-    );
-    let [policy, setPolicy] = React.useState("");
-    let [tName, setTName] = React.useState("");
-    let [tNum, setTNum] = React.useState('0');
-    let [walletAssets, setWalletAssets] = React.useState([{}]);
-   
-    const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const addRequest = useStoreActions(
+    (actions) => actions.requests.addRequest
+  );
+  let [policy, setPolicy] = React.useState("");
+  let [tName, setTName] = React.useState("");
+  let [tNum, setTNum] = React.useState('0');
+  let [walletAssets, setWalletAssets] = React.useState([{}]);
 
-    const PolicyInput = () => {
-      [policy, setPolicy] = React.useState("");
-      const handleChange = event => setPolicy(event.target.value);
-    
-      return (
-        <>
-          <Input
-            value={policy}
-            onChange={handleChange}
-            placeholder="Policy ID"
-          />
-        </>
-      );
-    };
-    
-    const TokenNameInput = () => {
-      [tName, setTName] = React.useState("");
-      const handleChange = event => setTName(event.target.value);
-    
-      return (
-        <>
-          <Input
-            value={tName}
-            onChange={handleChange}
-            placeholder="Token Name"
-          />
-        </>
-      );
-    };
+  const toast = useToast();
 
-    const loadWalletAssets = async () => {
-      const addr1 = await addressToBech32()
-      if(addr1){ 
-        return await getAllAssets(addr1)
-      }
-    }
-    
-    const NumberOfAsset = () => {
-      [tNum, setTNum] = React.useState('0');
-      const parse = (val) => val.replace(/^\$/, '')
-    
-      return (
-        <NumberInput
-          onChange={(valueString) => setTNum(parse(valueString))}
-          value={tNum}
-        >
-          <NumberInputField />
-        </NumberInput>
-      )
-    }
-    
-    const chooseAsset = (asName) => {
-      console.log("chooseAsset")
-      console.log(asName)
-      var ast = walletAssets.find(wa => typeof(wa) !== "undefined" && wa.name == asName)
-      if(ast && ast.name && ast.encodedFullName) {
-        setTName(ast.name)
-        setPolicy(ast.encodedFullName.substring(0,56))
-      }
-    }
-
-    const addAssets = () => {
-      addRequest({ // TODO - We need to make this use unit and quantity as the offer function expects so that we can shoot it over effectively. That way it converts nicely.
-        // TODO - We may also want to take into consideration ADA values here without indvidiuals inputting the policy "lovelace" with no token name will give you ada.
-        // 1/1M of an ADA.
-        unit: policy + fromAscii(tName),
-        quantity: tNum
-      })
-    }
-
-    const getImage = (asset) => {
-      if(asset != '') {
-        return `https://ipfs.blockfrost.dev/ipfs/${asset}` 
-      } else {
-        return "/somedefaultpic.png"
-      }
-    }
+  const PolicyInput = () => {
+    [policy, setPolicy] = React.useState("");
+    const handleChange = event => setPolicy(event.target.value);
 
     return (
       <>
-        <Button onClick={onOpen}>Select Assets to Send to Treasury</Button>
-
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Add an asset for the Treasury</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <PolicyInput/>
-              <TokenNameInput/>
-              <NumberOfAsset/>
-              <p>Please input the number desired above. This form is for your requested value.</p>
-              <>
-                <Button variant='ghost' onClick={addAssets}>Add Asset to Offer</Button>
-                <Button onClick={() => loadWalletAssets().then(data => {
-                setWalletAssets(data)
-                console.log("data",data)
-                // console.log(walletAssets)
-                // let ast = walletAssets.find(wa => typeof(wa) !== "undefined" && wa.name == asset.name)
-                })}>Load assets from a wallet</Button>
-                <SimpleGrid maxH="500px" w="400px" justifyContent="center" columns={2} overflowX={"hidden"} overflowY="scroll">
-                  {walletAssets.filter(x => typeof(x) !== "undefined" && x.name).map((asset) => (
-                      <Flex w="150px" direction={"column"} p={1} _hover={{cursor: "pointer"}} onClick={() => chooseAsset(asset.name)}>
-                        <Image src={getImage(asset.image)} w="150px" h="150px"></Image>
-                        <Flex mx="auto" my={1}>{hex_to_ascii(fromAscii(asset.name))}</Flex>
-                        {/* <Heading as="h4" mx="auto" my={1}>{asset.name}</Heading> */}
-                      </Flex>
-                  ))}
-                </SimpleGrid>
-              </>
-            </ModalBody>
-
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={onClose}>
-                Close
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+        <Input
+          value={policy}
+          onChange={handleChange}
+          placeholder="Policy ID"
+        />
       </>
     );
+  };
+
+  const TokenNameInput = () => {
+    [tName, setTName] = React.useState("");
+    const handleChange = event => setTName(event.target.value);
+
+    return (
+      <>
+        <Input
+          value={tName}
+          onChange={handleChange}
+          placeholder="Token Name"
+        />
+      </>
+    );
+  };
+
+  const loadWalletAssets = async () => {
+    const addr1 = await addressToBech32()
+    if (addr1) {
+      return await getAllAssets(addr1)
+    }
   }
+
+  const NumberOfAsset = () => {
+    [tNum, setTNum] = React.useState('0');
+    const parse = (val) => val.replace(/^\$/, '')
+    return (
+      <NumberInput
+        onChange={(valueString) => {
+          let maxQuantity = 1
+          let selectedFullName = policy + tName
+          walletAssets.forEach((asset) => {
+            maxQuantity = asset.encodedFullName === selectedFullName ? maxQuantity = asset.quantity : 1
+          })
+          let selectedQuantity = parse(valueString) > maxQuantity ? maxQuantity : parse(valueString)
+          setTNum(selectedQuantity)
+          }
+        }
+        value={tNum}
+      >
+        <NumberInputField />
+      </NumberInput>
+    )
+  }
+
+  const chooseAsset = (asName) => {
+    console.log("chooseAsset")
+    console.log(asName)
+    var ast = walletAssets.find(wa => typeof (wa) !== "undefined" && wa.name == asName)
+    if (ast && ast.name && ast.encodedFullName) {
+      setTName(ast.name)
+      setPolicy(ast.encodedFullName.substring(0, 56))
+      setTNum(1)
+    }
+  }
+
+  const addAssets = () => {
+    addRequest({ // TODO - We need to make this use unit and quantity as the offer function expects so that we can shoot it over effectively. That way it converts nicely.
+      // TODO - We may also want to take into consideration ADA values here without indvidiuals inputting the policy "lovelace" with no token name will give you ada.
+      // 1/1M of an ADA.
+      unit: policy + fromAscii(tName),
+      quantity: tNum
+    })
+  }
+
+  const getImage = (asset) => {
+    if (asset != '') {
+      return `https://ipfs.blockfrost.dev/ipfs/${asset}`
+    } else {
+      return "/somedefaultpic.png"
+    }
+  }
+
+  return (
+    <>
+      <Button onClick={onOpen}>Select Assets to Send to Treasury</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add an asset for the Treasury</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <PolicyInput />
+            <TokenNameInput />
+            <NumberOfAsset />
+            <p>Please input the number desired above. This form is for your requested value.</p>
+            <>
+              <Button variant='ghost' onClick={addAssets}>Add Asset to Offer</Button>
+              <Button onClick={() => loadWalletAssets().then(data => {
+                setWalletAssets(data)
+                console.log("data", data)
+                // console.log(walletAssets)
+                // let ast = walletAssets.find(wa => typeof(wa) !== "undefined" && wa.name == asset.name)
+              })}>Load assets from a wallet</Button>
+              <SimpleGrid maxH="500px" w="400px" justifyContent="center" columns={2} overflowX={"hidden"} overflowY="scroll">
+                {walletAssets.filter(x => typeof (x) !== "undefined" && x.name).map((asset) => (
+                  <Flex w="150px" direction={"column"} p={1} _hover={{ cursor: "pointer" }} onClick={() => chooseAsset(asset.name)}>
+                    <Image src={getImage(asset.image)} w="150px" h="150px"></Image>
+                    <Flex mx="auto" my={1}>{hex_to_ascii(fromAscii(asset.name))}</Flex>
+                    {/* <Heading as="h4" mx="auto" my={1}>{asset.name}</Heading> */}
+                  </Flex>
+                ))}
+              </SimpleGrid>
+            </>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 export default RequestSelection;
